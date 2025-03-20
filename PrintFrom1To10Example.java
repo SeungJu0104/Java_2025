79c4178065c881ea9b6cf3f8de91d89c9430b810
남승주 <skatmdwn23@naver.com>
2025-03-20 12:59:04 +0900

    조건문, 반복문, do-while문 실습
    조건문, 반복문 이용한 합 구하기, do-while문 실습

--- PrintFrom1To10Example.java
+++ PrintFrom1To10Example.java
@@ -1,9 +1,11 @@
-package ch04.sec04;
+package ch04.sec05;
 
 public class PrintFrom1To10Example {
 	public static void main(String[] args) {
-		for(int i=1; i<=10; i++) {
+		int i = 1;
+		while (i<=10) {
 			System.out.print(i + " ");
+			i++;
 		}
 	}
 }
--- SumFrom1To100Example.java
+++ SumFrom1To100Example.java
@@ -1,14 +1,16 @@
-package ch04.sec04;
+package ch04.sec05;
 
 public class SumFrom1To100Example {
 	public static void main(String[] args) {
 		int sum = 0;
-		int i;
-			
-		for(i=1; i<=100; i++) {
+		
+		int i = 1;
+		
+		while(i<=100) {
 			sum += i;
+			i++;
 		}
-			
+
 		System.out.println("1~" + (i-1) + " 합 : " + sum);
 	}
- }
+}
