4ad16f8bb9ad0f92415bcfe0775030fabf71c1a4
남승주 <skatmdwn23@naver.com>
2025-04-09 16:49:33 +0900

    정적 메소드에서 인스턴스 멤버 호출
    정적 메소드와 인스턴스 멤버 관련 실습 수행.

--- Car.java
+++ Car.java
@@ -1,35 +1,31 @@
-package ch06.sec07.exam05;
+package ch06.sec10.exam03;
 
 public class Car {
-	// 필드 선언
-	String company = "현대자동차";
-	String model;
-	String color;
-	int maxSpeed;
-	
-	Car(String model) {
-		//20라인 생성자 호출
-		this(model, "은색", 250);
+	//인스턴스 필드 선언
+	int speed;
+
+	//인스턴스 메소드 선언
+	void run() {
+		System.out.println(speed + "으로 달립니다.");
 	}
 
-	Car(String model, String color) {
-		//20라인 생성자 호출
-		this(model, color, 250);
-	}
-	
-	// 생성자 오버로딩 시 서로 다른 변수의 순서 차이가 있다면 가능하다.
-	// 동일 변수의 순서차이는 불가능하다.
-	Car(String color, int maxSpeed){
-		this(null, color, maxSpeed);
-	}
-	
-	Car(int maxSpeed, String color){
-		this(null, color, maxSpeed);
+	static void simulate() {
+		//객체 생성
+		Car myCar = new Car();
+		//인스턴스 멤버 사용
+		myCar.speed = 200;
+		myCar.run();
 	}
 
-	Car(String model, String color, int maxSpeed) {
-		this.model = model;
-		this.color = color;
-		this.maxSpeed = maxSpeed;
+	public static void main(String[] args) {
+		//정적 메소드 호출(묵시적)
+		simulate();
+		// 명시적인 Car.simulate(); 로 사용하는 것이 좋다.
+		
+		//객체 생성
+		Car myCar = new Car();
+		//인스턴스 멤버 사용
+		myCar.speed = 60;
+		myCar.run();
 	}
 }
