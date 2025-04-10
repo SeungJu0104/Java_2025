09ba0252d1fea91a63cea8f3cf4ac94f6b4daa3a
남승주 <skatmdwn23@naver.com>
2025-04-10 21:41:27 +0900

    import문
    import 관련 실습 수행.

--- Car.java
+++ Car.java
@@ -1,31 +1,17 @@
-package ch06.sec10.exam03;
+package ch06.sec12.hyundai;
 
+//import 문으로 다른 패키지 클래스 사용을 명시
+//다른 패키지의 클래스 사용하려면 반드시 import 해야한다.
+import ch06.sec12.hankook.SnowTire;
+import ch06.sec12.hankook.Tire;
+import ch06.sec12.kumho.AllSeasonTire;
+// import ch06.sec12.hankook.*; // hankook 패키지 내 모든 클래스 파일 import
+	
 public class Car {
-	//인스턴스 필드 선언
-	int speed;
-
-	//인스턴스 메소드 선언
-	void run() {
-		System.out.println(speed + "으로 달립니다.");
-	}
-
-	static void simulate() {
-		//객체 생성
-		Car myCar = new Car();
-		//인스턴스 멤버 사용
-		myCar.speed = 200;
-		myCar.run();
-	}
-
-	public static void main(String[] args) {
-		//정적 메소드 호출(묵시적)
-		simulate();
-		// 명시적인 Car.simulate(); 로 사용하는 것이 좋다.
-		
-		//객체 생성
-		Car myCar = new Car();
-		//인스턴스 멤버 사용
-		myCar.speed = 60;
-		myCar.run();
-	}
+	//부품 필드 선언
+	ch06.sec12.hankook.Tire tire1 = new ch06.sec12.hankook.Tire(); // import 한 패키지의 풀네임을 작성해 Tire 클래스 객체 생성
+	ch06.sec12.kumho.Tire tire2 = new ch06.sec12.kumho.Tire();
+	Tire tire5 = new Tire(); // import한 패키지는 줄여쓸 수 있다.
+	SnowTire tire3 = new SnowTire();
+	AllSeasonTire tire4 = new AllSeasonTire(); // 작성 후 ctrl + shift + o 를 누르면 패키지 import 할 수 있다.
 }
---
+++ Tire.java
@@ -0,0 +1,4 @@
+package ch06.sec12.hankook;
+// 파일을 복사할 경우 패키지도 동일하게 변경해줘야한다.
+public class Tire {
+}
