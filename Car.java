8710ec93300cd1d1b1940706b8d702d89a5b6695
남승주 <skatmdwn23@naver.com>
2025-04-14 21:29:17 +0900

    Getter / Setter
    Getter / Setter를 직접 작성해보고, 이클립스에서 자동 생성하는 것까지 실습 수행.

--- Car.java
+++ Car.java
@@ -1,17 +1,28 @@
-package ch06.sec12.hyundai;
+package ch06.sec14;
 
-//import 문으로 다른 패키지 클래스 사용을 명시
-//다른 패키지의 클래스 사용하려면 반드시 import 해야한다.
-import ch06.sec12.hankook.SnowTire;
-import ch06.sec12.hankook.Tire;
-import ch06.sec12.kumho.AllSeasonTire;
-// import ch06.sec12.hankook.*; // hankook 패키지 내 모든 클래스 파일 import
-	
 public class Car {
-	//부품 필드 선언
-	ch06.sec12.hankook.Tire tire1 = new ch06.sec12.hankook.Tire(); // import 한 패키지의 풀네임을 작성해 Tire 클래스 객체 생성
-	ch06.sec12.kumho.Tire tire2 = new ch06.sec12.kumho.Tire();
-	Tire tire5 = new Tire(); // import한 패키지는 줄여쓸 수 있다.
-	SnowTire tire3 = new SnowTire();
-	AllSeasonTire tire4 = new AllSeasonTire(); // 작성 후 ctrl + shift + o 를 누르면 패키지 import 할 수 있다.
+	//필드 선언
+	private int speed;
+	private boolean stop;
+	
+	//speed 필드의 Getter/Setter 선언
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		if(speed < 0) {
+			this.speed = 0;
+			return;
+		} else {
+			this.speed = speed;
+		}
+	}
+	//stop 필드의 Getter/Setter 선언
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		if(stop == true) this.speed = 0;
+	}
 }
--- CarExample.java
+++ CarExample.java
@@ -1,22 +1,22 @@
-package ch06.sec07.exam05;
+package ch06.sec14;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car car1 = new Car("자가용");
-		System.out.println("car1.company : " + car1.company);
-		System.out.println("car1.model : " + car1.model);
-		System.out.println();
+		//객체 생성
+		Car myCar = new Car();
 
-		Car car2 = new Car("자가용", "빨강");
-		System.out.println("car2.company : " + car2.company);
-		System.out.println("car2.model : " + car2.model);
-		System.out.println("car2.color : " + car2.color);
-		System.out.println();
+		//잘못된 속도 변경
+		myCar.setSpeed(-50);
+		System.out.println("현재 속도: " + myCar.getSpeed());
+
+		//올바른 속도 변경
+		myCar.setSpeed(60);
+		System.out.println("현재 속도: " + myCar.getSpeed());
 		
-		Car car3 = new Car("택시", "검정", 200);
-		System.out.println("car3.company : " + car3.company);
-		System.out.println("car3.model : " + car3.model);
-		System.out.println("car3.color : " + car3.color);
-		System.out.println("car3.maxSpeed : " + car3.maxSpeed);
+		//멈춤
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		System.out.println("현재 속도: " + myCar.getSpeed());
 	}
 }
