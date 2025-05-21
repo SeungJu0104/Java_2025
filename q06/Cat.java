97dfdf84d10883c4a0422316c390e7f5b0f0e725
남승주 <skatmdwn23@naver.com>
2025-05-21 19:11:24 +0900

    인터페이스 확인 문제 6번
    6번 문제 풀이

---
+++ q06/Cat.java
@@ -0,0 +1,10 @@
+package checkquiz.q06;
+
+public class Cat implements Soundable{
+
+	@Override
+	public String sound() {
+		return "야옹";
+	}
+
+}
---
+++ q06/Soundable.java
@@ -0,0 +1,5 @@
+package checkquiz.q06;
+
+public interface Soundable {
+	public String sound();
+}
