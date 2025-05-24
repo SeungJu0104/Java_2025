fb8f042c4f8a6882ed98c02bc7986f03cb808b0f
남승주 <skatmdwn23@naver.com>
2025-05-24 21:47:12 +0900

    로컬 클래스
    로컬 필드, 생성자, 정적 메소드, 로컬 메소드 생성 실습 수행.

---
+++ sec04/exam02/AExample.java
@@ -0,0 +1,14 @@
+package ch09.sec04.exam02;
+
+public class AExample {
+	public static void main(String[] args) {
+		//A 객체 생성
+		//A a = new A();
+
+		//A 메소드 호출
+		//a.useB();
+		
+		// A 메소드 호출
+		new A().useB(); // A 객체 만들어 함수 실행하고, B 객체 생성해 생성자로 초기화. method1 실행. method2 실행.
+	}
+}
