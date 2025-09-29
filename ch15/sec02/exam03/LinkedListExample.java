7f60b10ba2b98e76426d6d3e4016ed9ff391c901
남승주 <skatmdwn23@naver.com>
2025-09-29 21:51:13 +0900

    LinkedList
    LinkedList 에 객체를 추가하고, 걸린 시간을 간단히 측정해보는 실습 수행.

---
+++ ch15/sec02/exam03/LinkedListExample.java
@@ -0,0 +1,36 @@
+package ch15.sec02.exam03;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+	public static void main(String[] args) {
+		//ArrayList 컬렉션 객체 생성
+		List<String> list1 = new ArrayList<String>();
+
+		//LinkedList 컬렉션 객체 생성
+		List<String> list2 = new LinkedList<String>();
+
+		//시작 시간과 끝 시간을 저장할 변수 선언
+		long startTime;
+		long endTime;
+
+		//ArrayList 컬렉션에 저장하는 시간 측정
+		startTime = System.nanoTime();
+		for(int i=0; i<50000; i++) {
+			list1.add(0, String.valueOf(i));
+		}
+		endTime = System.nanoTime();
+		System.out.printf("%-17s %8d ns \n", "ArrayList 걸린 시간: ", (endTime-startTime) );
+
+		//LinkedList 컬렉션에 저장하는 시간 측정
+		// 데이터 복사, 이동 없이 앞 뒤 객체의 연결만 끊고 새로 생성하면 되기때문에 ArrayList보다 시간 많이 우위
+		startTime = System.nanoTime();
+		for(int i=0; i<50000; i++) {
+			list2.add(0, String.valueOf(i));
+		}
+		endTime = System.nanoTime();
+		System.out.printf("%-17s %8d ns \n", "LinkedList 걸린 시간: ", (endTime-startTime) );
+	}
+}
