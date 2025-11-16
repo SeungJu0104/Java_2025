ca06581daaffe2ffa9167a6c20be792bb5578877
남승주 <skatmdwn23@naver.com>
2025-11-16 21:23:22 +0900

    루핑
    peek와 forEach 를 사용해 정수 짝수 스트림 요소를 반복 처리하는 실습 수행.

---
+++ ch17/sec08/LoopingExample.java
@@ -0,0 +1,33 @@
+package ch17.sec08;
+
+import java.util.Arrays;
+	
+public class LoopingExample {
+	public static void main(String[] args) {
+		int[] intArr = { 1, 2, 3, 4, 5 };
+
+		//잘못 작성한 경우
+		Arrays.stream(intArr)
+			.filter(a -> a%2==0)
+			.peek(n -> System.out.println(n)); 		//최종 처리가 없으므로 동작하지 않음
+			// peek은 중간 스트림 중 하나
+		
+		//중간 처리 메소드 peek()을 이용해서 반복 처리
+		int total = Arrays.stream(intArr)
+			.filter(a -> a%2==0)
+			.peek(n -> System.out.println(n)) 		//동작함
+			.sum(); //최종 처리
+		System.out.println("총합: " + total + "\n");
+
+		//최종 처리 메소드 forEach()를 이용해서 반복 처리
+		Arrays.stream(intArr)
+			.filter(a -> a%2==0)
+			.forEach(n -> System.out.println(n)); 	//최종 처리이므로 동작함
+			//.sum(); -> 앞에서 최종 처리했기때문에 sum으로 또 다른 최종 처리 할 수 없다.(최종 처리는 1번만 가능)
+		
+		// 이전 스트림 객체는 사용할 수 없으므로 다시 생성해야한다.
+//		total.filter(a -> a%2==0)
+//		.peek(n -> System.out.println(n)) 		//동작함
+//		.sum(); //최종 처리
+	}
+}
